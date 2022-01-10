7c27eafba6c0940843e3092b67e9063179661eff
Reg-Sherman <reginaxsherman@gmail.com>
2022-01-10 09:09:09 -0600

    cleaned up code

--- js/main2.js
+++ js/main2.js
@@ -19,6 +19,8 @@ const cupNumber = Object.keys(numberOfStones) //get an array of the cupNumber of
 const myCups = [cupNumber[0], cupNumber[1], cupNumber[2], cupNumber[3], cupNumber[4], cupNumber[5]] // assigns only these cups to player
 const computerCups = [cupNumber[12], cupNumber[11], cupNumber[10], cupNumber[9], cupNumber[8], cupNumber[7]]
 let previousChoice = 'seven'
+
+
 //map over each key in the score object
 Object.keys(numberOfStones).forEach((id) => {
     const score = numberOfStones[id] //get the value for that id
@@ -39,18 +41,18 @@ myCups.forEach((cup) => {
         
         displayWinner()
         computerChoice = getComputerChoice()
-        console.log('computer choice: ', computerChoice)
         runLoop(computerChoice, false)
-        // setTimeout(() => runLoop(getComputerChoice()), 2000)
+        
         resetSquareColor(previousChoice)
         changeSquareColor(computerChoice)
         previousChoice = computerChoice
+        
+        
 
         Object.keys(numberOfStones).forEach((cup) => {
             const updatedCup = document.getElementById(cup)
             updatedCup.innerHTML = numberOfStones[cup]
         })
-        
         displayWinner()
         
     })
@@ -126,19 +128,9 @@ isBottomRowEmpty = () => {
 
 isGameOver = () => {
     if (isTopRowEmpty() === true  || isBottomRowEmpty() === true) {
-    return true 
+        return true 
     } else {
         return false
-    }
-}
-
-whoWon = () => {
-    if (numberOfStones.p1 > numberOfStones.cpu) {
-        return "You won!"
-    } else if (numberOfStones.p1 < numberOfStones.cpu) {
-        return "Computer wins"
-    } else if (numberOfStones.p1 == numberOfStones.cpu) {
-        return "Tie!"
     }
 }
 
@@ -147,68 +139,11 @@ displayWinner = () => {
         document.querySelector('#player').style.display = 'flex'
     } else if (isGameOver() === true && (numberOfStones.p1 < numberOfStones.cpu)) {
         document.querySelector('#computer').style.display = 'flex'
+    } else if (isGameOver() === true && (numberOfStones.p1 === numberOfStones.cpu)) {
+        document.querySelector('#tie').style.display = 'flex'
     }
 }
 
 hideInstructions = () => {
     document.querySelector('#h1').style.display = 'none'
 }
-
-
-
-//BUG: THIS WOULD CAUSE YOUR FUNCTION TO RUN WITHOUT WAITING FOR A USER TO CLICK
-//locationOfFirstDiv.addEventListener("click", runLoop()) <-- this will not listen for an event but run immediately: wrong syntax
-
-//Now, it's the next players turn and they can choose where to start, meaning we make use of a event listener that listen for which cup a user selects
-//Each cup will have an event listener
-
-//Edge case: How to check what cups a player can pick from:
-//Example: Let's say we are the player and our side is the bottom side we need check which cups are valid:
-//If statement: "If ((what the user selected with the event listener is === cup[1]) || (what the user selected === cup [2]) || (etc..)"
-
-// If ((topRow.includes(the div that I click) {
-    //allow click and run eventListner
-    // } else {
-        //do not allow click and show alert "choose from the other row"
-        // })
-        
-        // if (topRow.includes('#one') === true) {
-            // }
-            // console.log(topRow.includes('#one'))
-
-
-//CODE ORGANIZATION:
-//top - to - bottom generally this is the flow. 
-
-//Having a notion constants: values need to be tracked throughout the game, we will initialized/declared at the top of our file
-//All our functions and the meat and potatoes of our logic is the middle
-//By convention, event listeners are kept at the very bottom of the JS file in the global scope.
-
-
-//NEXT STEPS: <-- Overall goal: This will involve setting up event listeners, dom manipulation, and updating the integer value for each div as the loop iterates
-//When the game renders for a player, each div/cup should have an event listener waiting for a click. 
-  //when the syntax is complete, check that a console log that says "this works!" outputs to the console.
-
-//The next place we can continue from here is working from inside the event listener.
-  //Once we can confirm the console log outputs, on the next call to your first function such as runLoop()
-  //Actually create this runLoop() function and the necessary code inside
-  //use console logs to check the state of the cups array to verify your loop works as intended
-
-//Once runLoop() works, I need to call another function to reset the stones, callback inside runLoop() to resetStones()
-  //Create the resetStones() function and the logic for it
-  //Check with console log
-
-//Can be stretch goal: Our next goal would be to use dom manipulation to display these values in the browser
-
-//Still need to compare values in cups[0] and cups [13] to see who wins
-    //Do I want to have the game end after a certain number of turns, or when one side is empty? 
-    //How do I determine when a whole side is empty? 
-
-//Distinguish sides so players can only choose from their side 
-
-//Automate computer player
-
-//Turn indicators
-
-//Keep track of how many are in each store? -- this will happen once I am able to display the array indices on each div 
-
